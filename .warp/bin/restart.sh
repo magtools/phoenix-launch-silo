f3178fa399183ad1ec61045a5afc4fe9f52bb8d9
martin <magtec@gmail.com>
2026-05-13 23:45:00 -0300

    warp restart service

--- .warp/bin/restart.sh
+++ .warp/bin/restart.sh
@@ -15,10 +15,24 @@
 
 ##-c ####################-c #################
 function restart_command() {
+  local _mode="${1:-}"
+  local _service="${2:-}"
 
   if [ "$1" = "-h" ] || [ "$1" = "--help" ] ; then
-        
       restart_help_usage
+  elif [ "$_mode" = "service" ] || [ "$_mode" = "-s" ] ; then
+    if [ -z "$_service" ]; then
+      warp_message_error "compose service name is required"
+      restart_help_usage
+      exit 1
+    fi
+
+    if [ ! -f "$DOCKERCOMPOSEFILE" ]; then
+      warp_message_error "$DOCKERCOMPOSEFILE not found"
+      exit 1
+    fi
+
+    docker-compose -f "$DOCKERCOMPOSEFILE" restart "$_service"
   else
     stop_main stop
     start_main start
